21850a6be9e59536e72ca1d59772915030c28d42
Renee Mei and said ahmed <said@>
2017-01-24 11:22:45 -0800

    add scripts so that when the selected time is before 4PM return 11 for price

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,20 @@ function Ticket(movie, age, time){
 }
 
 Ticket.prototype.price = function() {
-  return this.movie
+  if (this.time === 1){
+    return 11;
+  }else if (this.time ===2){
+    return this.movie
+  }
 }
 
 //front-end logics
 $(document).ready(function(){
   $("form.movie-tickets").submit(function(event){
     event.preventDefault();
-    var inputtedMovie = $("#movie option:selected").val();
-    var inputtedAge = $("#movie option:selected").val();
-    var inputtedTime = $("#movie option:selected").val();
+    var inputtedMovie = parseInt($("#movie option:selected").val());
+    var inputtedAge = parseInt($("#age option:selected").val());
+    var inputtedTime = parseInt($("#time option:selected").val());
     var newTicket = new Ticket(inputtedMovie, inputtedAge, inputtedTime);
 
     $("#price").text(newTicket.price());
